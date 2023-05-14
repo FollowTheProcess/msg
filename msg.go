836360b5619ae3bdca0b03b28876224b44f52676
Tom Fleet <tomfleet2018@gmail.com>
2023-05-14 10:47:10 +0100

    Add success, error and warn messages

--- msg.go
+++ msg.go
@@ -1,7 +1,82 @@
-// Package msg is a placeholder for something cool.
+// Package msg is a simple, easy to use, opinionated console printing toolkit for Go CLIs rendering pretty
+// formatted output with colours specific to the particular message type:
+//
+//   - Info: For general user information and progress updates
+//   - Title: Separation between sections of output
+//   - Warn: User warnings
+//   - Success: Report success
+//   - Error: Report failure
+//
+// All message types default to stdout other than the `Error` type which prints to stderr by default.
+//
+// There are also "F-style" print methods that allow you to specify an [io.Writer] to print the messages to.
 package msg
 
-// Hello returns a welcome message for the project.
-func Hello() string {
-	return "Hello msg"
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+const (
+	// Default colors for each message type.
+	colorInfo    = color.FgHiCyan
+	colorTitle   = color.FgCyan
+	colorWarn    = color.FgYellow
+	colorError   = color.FgHiRed
+	colorSuccess = color.FgGreen
+
+	// Default statuses for each message type.
+	statusInfo    = "Info"
+	statusWarn    = "Warning"
+	statusError   = "Error"
+	statusSuccess = "Success"
+)
+
+// Success prints a success message with optional format args to stdout.
+//
+//	msg.Success("Compiled project: %s", "msg")
+func Success(format string, a ...any) {
+	Fsuccess(os.Stdout, format, a...)
+}
+
+// Fsuccess prints a success message with optional format args to w.
+func Fsuccess(w io.Writer, format string, a ...any) {
+	success := color.New(colorSuccess, color.Bold)
+	message := color.New(color.FgHiWhite)
+
+	fmt.Fprintf(w, "%s: %s\n", success.Sprint(statusSuccess), message.Sprintf(format, a...))
+}
+
+// Error prints an error message with optional format args to stderr.
+//
+//	msg.Error("Invalid config")
+//	msg.Error("Could not find file: %s", "missing.txt")
+func Error(format string, a ...any) {
+	Ferror(os.Stderr, format, a...)
+}
+
+// Ferror prints an error message with optional format args to w.
+func Ferror(w io.Writer, format string, a ...any) {
+	e := color.New(colorError, color.Bold)
+	message := color.New(color.FgHiWhite)
+
+	fmt.Fprintf(w, "%s: %s\n", e.Sprint(statusError), message.Sprintf(format, a...))
+}
+
+// Warn prints a warning message with optional format args to stdout.
+//
+//	msg.Warn("Skipping %s, directory is empty", "some/empty/dir")
+func Warn(format string, a ...any) {
+	Fwarn(os.Stdout, format, a...)
+}
+
+// Fwarn prints a warning message with optional format args to w.
+func Fwarn(w io.Writer, format string, a ...any) {
+	warn := color.New(colorWarn, color.Bold)
+	message := color.New(color.FgHiWhite)
+
+	fmt.Fprintf(w, "%s: %s\n", warn.Sprint(statusWarn), message.Sprintf(format, a...))
 }
--- msg_test.go
+++ msg_test.go
@@ -1,16 +1,145 @@
 package msg_test
 
 import (
+	"bytes"
+	"io"
+	"os"
 	"testing"
 
 	"github.com/FollowTheProcess/msg"
 )
 
-func TestHello(t *testing.T) {
-	got := msg.Hello()
-	want := "Hello msg"
+func TestSuccess(t *testing.T) {
+	buf := new(bytes.Buffer)
+	msg.Fsuccess(buf, "Something went well: %v", 42)
+
+	want := "Success: Something went well: 42\n"
+
+	if got := buf.String(); got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestSuccessCaptured(t *testing.T) {
+	successFunc := func() {
+		msg.Success("Worked")
+	}
+	got := captureStdout(t, successFunc)
+	want := "Success: Worked\n"
 
 	if got != want {
-		t.Errorf("got %s, wanted %s", got, want)
+		t.Errorf("got %q, wanted %q", got, want)
 	}
+}
+
+func TestError(t *testing.T) {
+	buf := new(bytes.Buffer)
+	msg.Ferror(buf, "Something broke: %v", true)
+
+	want := "Error: Something broke: true\n"
+
+	if got := buf.String(); got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestErrorCaptured(t *testing.T) {
+	errorFunc := func() {
+		msg.Error("Bad number (%v)", 42)
+	}
+	got := captureStderr(t, errorFunc)
+	want := "Error: Bad number (42)\n"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestWarn(t *testing.T) {
+	buf := new(bytes.Buffer)
+	msg.Fwarn(buf, "skipping directory %s", "./tmp")
+
+	want := "Warning: skipping directory ./tmp\n"
+
+	if got := buf.String(); got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func TestWarnCaptured(t *testing.T) {
+	warnFunc := func() {
+		msg.Warn("Skipping something (%d)", 42)
+	}
+	got := captureStdout(t, warnFunc)
+	want := "Warning: Skipping something (42)\n"
+
+	if got != want {
+		t.Errorf("got %q, wanted %q", got, want)
+	}
+}
+
+func captureStdout(t *testing.T, printer func()) string {
+	t.Helper()
+	old := os.Stdout // Backup of the real one
+	defer func() {
+		os.Stdout = old // Set it back even if we error later
+	}()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("os.Pipe() returned an error: %v", err)
+	}
+
+	// Set stdout to our new pipe
+	os.Stdout = w
+
+	capture := make(chan string)
+	// Copy in a goroutine so printing can't block forever
+	go func() {
+		buf := new(bytes.Buffer)
+		io.Copy(buf, r) //nolint: errcheck
+		capture <- buf.String()
+	}()
+
+	// Call our test function that prints to stdout
+	printer()
+
+	// Close the writer
+	w.Close()
+	captured := <-capture
+
+	return captured
+}
+
+func captureStderr(t *testing.T, printer func()) string {
+	t.Helper()
+	old := os.Stderr // Backup of the real one
+	defer func() {
+		os.Stderr = old // Set it back even if we error later
+	}()
+
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatalf("os.Pipe() returned an error: %v", err)
+	}
+
+	// Set stderr to our new pipe
+	os.Stderr = w
+
+	capture := make(chan string)
+	// Copy in a goroutine so printing can't block forever
+	go func() {
+		buf := new(bytes.Buffer)
+		io.Copy(buf, r) //nolint: errcheck
+		capture <- buf.String()
+	}()
+
+	// Call our test function that prints to stderr
+	printer()
+
+	// Close the writer
+	w.Close()
+	captured := <-capture
+
+	return captured
 }
