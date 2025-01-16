826b24b7cf029b839b097a8f2f6ea07df7b686eb
Tom Fleet <tomfleet2018@gmail.com>
2025-01-16 15:37:25 +0000

    Make `Err` do a pretty cause tree (#87)

--- msg_test.go
+++ msg_test.go
@@ -15,6 +15,7 @@ import (
 const (
 	successCode = "\x1b[1;32m"
 	errorCode   = "\x1b[1;31m"
+	causeCode   = "\x1b[1m"
 	warnCode    = "\x1b[1;33m"
 	infoCode    = "\x1b[1;36m"
 	titleCode   = "\x1b[96m"
@@ -75,28 +76,81 @@ func TestErrorCaptured(t *testing.T) {
 }
 
 func TestErr(t *testing.T) {
-	buf := new(bytes.Buffer)
-	err := errors.New("Something broke")
-	msg.Ferr(buf, err)
+	t.Run("nil", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		var err error = nil
+		msg.Ferr(buf, err)
 
-	want := fmt.Sprintf("%sError%s: Something broke\n", errorCode, resetCode)
+		if buf.Len() != 0 {
+			t.Fatalf("nil err wrote output: %s", buf.String())
+		}
+	})
+	t.Run("plain", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		err := errors.New("Something broke")
+		msg.Ferr(buf, err)
 
-	if got := buf.String(); got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+		want := fmt.Sprintf("%sError%s: Something broke\n", errorCode, resetCode)
+
+		if got := buf.String(); got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
+
+	t.Run("wrapped", func(t *testing.T) {
+		buf := new(bytes.Buffer)
+		root := errors.New("something broke")
+		one := fmt.Errorf("could not frobnicate the baz: %w", root)
+		two := fmt.Errorf("failed to process file: %w", one)
+		three := fmt.Errorf("could not deposit money: %w", two)
+		msg.Ferr(buf, three)
+		wantTemplate := `%[1]sError%[2]s: could not deposit money
+╰─ %[3]scause%[2]s: failed to process file
+   ╰─ %[3]scause%[2]s: could not frobnicate the baz
+      ╰─ %[3]scause%[2]s: something broke
+`
+
+		want := fmt.Sprintf(wantTemplate, errorCode, resetCode, causeCode)
+
+		if got := buf.String(); got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
 }
 
 func TestErrCaptured(t *testing.T) {
-	errorFunc := func() {
-		err := errors.New("Bang!")
-		msg.Err(err)
-	}
-	got := captureStderr(t, errorFunc)
-	want := fmt.Sprintf("%sError%s: Bang!\n", errorCode, resetCode)
+	t.Run("plain", func(t *testing.T) {
+		errorFunc := func() {
+			err := errors.New("Bang!")
+			msg.Err(err)
+		}
+		got := captureStderr(t, errorFunc)
+		want := fmt.Sprintf("%sError%s: Bang!\n", errorCode, resetCode)
 
-	if got != want {
-		t.Errorf("got %q, wanted %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
+	t.Run("wrapped", func(t *testing.T) {
+		errorFunc := func() {
+			root := errors.New("bang!")
+			one := fmt.Errorf("dingle cannot dangle on version <2: %w", root)
+			two := fmt.Errorf("could not read file: %w", one)
+			three := fmt.Errorf("you have no money: %w", two)
+			msg.Err(three)
+		}
+		wantTemplate := `%[1]sError%[2]s: you have no money
+╰─ %[3]scause%[2]s: could not read file
+   ╰─ %[3]scause%[2]s: dingle cannot dangle on version <2
+      ╰─ %[3]scause%[2]s: bang!
+`
+		got := captureStderr(t, errorFunc)
+		want := fmt.Sprintf(wantTemplate, errorCode, resetCode, causeCode)
+
+		if got != want {
+			t.Errorf("got %q, wanted %q", got, want)
+		}
+	})
 }
 
 func TestWarn(t *testing.T) {
@@ -166,6 +220,22 @@ func TestTitleCaptured(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, wanted %q", got, want)
 	}
+}
+
+func TestVisual(t *testing.T) {
+	msg.Title("Your CLI output:")
+	msg.Success("compiled 42 packages")
+	msg.Warn("directory is empty, skipping")
+	msg.Info("using value from config file")
+
+	fmt.Println()
+
+	err := errors.New("bad file permissions")
+	one := fmt.Errorf("could not read DB config: %w", err)
+	two := fmt.Errorf("failed to insert new record: %w", one)
+	three := fmt.Errorf("could not complete transaction: %w", two)
+
+	msg.Err(three)
 }
 
 func captureStdout(t *testing.T, printer func()) string {
